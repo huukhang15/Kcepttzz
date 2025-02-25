5d66022a331ef39c3997758b0719ba6af61b0934
huukhang15 <huukhang.info@gmail.com>
2025-02-25 12:22:44 +0700

    test

--- new.py
+++ new.py
@@ -1,20 +1,28 @@
+import subprocess
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import requests
+
 #========= Cấu hình link =========#
 TIKTOK_URL = "https://www.tiktok.com/"
 FOLLOWING_URL = "https://www.tiktok.com/following"
 BRAVE_PATH = r"C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe"
 USER_URL = "https://www.tiktok.com/@{user}"
-#========Cấu hình brave=========
+
+#======== Cấu hình brave =========#
 def chay_brave():
     subprocess.Popen(f'"{BRAVE_PATH}" --incognito --remote-debugging-port=9222', shell=True)
 
+# Chạy Brave lần đầu
 time.sleep(1)  # Chờ một chút trước khi mở trình duyệt
 chay_brave()
+
 #========= Cấu hình Selenium =========#
 options = Options()
 options.binary_location = BRAVE_PATH
 options.add_argument("--mute-audio")
 options.add_argument("--disable-sound")
 options.debugger_address = "127.0.0.1:9222"
-
 driver = webdriver.Chrome(options=options)
 chay_brave()
