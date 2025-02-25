1045f8e78caf435890726e3098a778c911fc745e
huukhang15 <huukhang.info@gmail.com>
2025-02-25 13:10:09 +0700

    Add BRAVE_PATH to fix NameError

--- new.py
+++ new.py
@@ -1,7 +1,9 @@
 import subprocess
 import time
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 import requests
 
 #========= Cấu hình link =========#
@@ -15,14 +17,11 @@ options.binary_location = BRAVE_PATH
 options.add_argument("--mute-audio")
 options.add_argument("--disable-sound")
 options.debugger_address = "127.0.0.1:9222"
-driver = webdriver.Chrome(options=options)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 #======== Cấu hình brave =========#
 def chay_brave():
-    # Đóng các phiên Brave đang chạy (nếu có)
     subprocess.call("taskkill /IM brave.exe /F", shell=True)
-    # Mở Brave với chế độ debug
     subprocess.Popen(f'"{BRAVE_PATH}" --incognito --remote-debugging-port=9222', shell=True)
-    # Chờ lâu hơn để đảm bảo Brave khởi động
     time.sleep(5)
 
 # Chạy Brave
