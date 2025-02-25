8c6fa9255dbb25a3b86917c17b2d577f52affd0c
huukhang15 <huukhang.info@gmail.com>
2025-02-25 13:02:42 +0700

    Add BRAVE_PATH to fix NameError

--- new.py
+++ new.py
@@ -9,15 +9,6 @@ TIKTOK_URL = "https://www.tiktok.com/"
 FOLLOWING_URL = "https://www.tiktok.com/following"
 BRAVE_PATH = r"C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe"
 USER_URL = "https://www.tiktok.com/@{user}"
-
-#======== Cấu hình brave =========#
-def chay_brave():
-    subprocess.Popen(f'"{BRAVE_PATH}" --incognito --remote-debugging-port=9222', shell=True)
-
-# Chạy Brave lần đầu
-time.sleep(1)  # Chờ một chút trước khi mở trình duyệt
-chay_brave()
-
 #========= Cấu hình Selenium =========#
 options = Options()
 options.binary_location = BRAVE_PATH
@@ -25,6 +16,15 @@ options.add_argument("--mute-audio")
 options.add_argument("--disable-sound")
 options.debugger_address = "127.0.0.1:9222"
 driver = webdriver.Chrome(options=options)
+#======== Cấu hình brave =========#
+def chay_brave():
+    subprocess.Popen(f'"{BRAVE_PATH}" --incognito --remote-debugging-port=9222', shell=True)
 
-# Mở trang TikTok
+# Chạy Brave lần đầu
+time.sleep(1)  # Chờ một chút trước khi mở trình duyệt
+chay_brave()
 driver.get(TIKTOK_URL)
+
+
+
+
