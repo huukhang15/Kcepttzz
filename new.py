266efc56e4d96dba42fb8be3b067f62daa7258d4
huukhang15 <157609439+huukhang15@users.noreply.github.com>
2025-02-26 10:27:24 +0700

    new.py

--- new.py
+++ new.py
@@ -179,7 +179,10 @@ if __name__ == "__main__":
         if cookies:
             gan_cookie_vao_brave(driver, cookies)
             time.sleep(5)
-            print("\n===== Đăng nhập thành công trên Brave! =====")
+            print("\033[F\033[K", end="")
+            print("\033[F\033[K", end="")
+            print("\033[F\033[K", end="")
+            print("\n===== Đăng nhập thành công ! =====")
             tiktok_login = TikTokLogin()
             cookie_final = '; '.join(cookies)
             result = tiktok_login.login_with_cookie(cookie_final)
